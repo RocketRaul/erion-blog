1b6aef8ec20b30167efcd19a00bbed5218b7a179
Raul Ojeda <raul.ojedacastillo@gmail.com>
2020-06-09 14:21:51 -0500

    Article Creation/Flash

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,5 +8,19 @@ class ArticlesController < ApplicationController
         @articles = Article.all
      end
 
+     def new
+        @article =Article.new
+     end
 
+     def create
+        @article =Article.new(params.require(:article).permit(:title, :description))
+        if @article.save
+            flash[:notice] ="Article was Created Successfully."
+        redirect_to @article
+        else
+            render 'new'
+        end
+
+     end
+     
 end
